20d08e6dd7c1a6fce05076c223743b72e18b940d
zhujunxi <531578783@qq.com>
2019-07-11 15:14:39 +0800

    [faet] nuxt demo

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,50 @@
 <template>
 	<div class="container">
-		<div>
-			<logo />
-			<h1 class="title">mvp</h1>
-			<h2 class="subtitle">My perfect Nuxt.js project</h2>
-			<div class="links">
-				<a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-				<a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-			</div>
-		</div>
+		<mt-header title="long long long long title" style="width:100%">
+			<router-link to="/" slot="left">
+				<mt-button icon="back">back</mt-button>
+			</router-link>
+			<mt-button icon="more" slot="right"></mt-button>
+		</mt-header>
+		{{content}}
+		<mt-button type="primary" @click="open">弹框</mt-button>
 	</div>
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
+import { Toast } from 'mint-ui'
 
 export default {
 	components: {
 		Logo
+	},
+	data() {
+		return {
+			selected: '1',
+			content: '内容'
+		}
+	},
+	methods: {
+		open() {
+			Toast({
+				message: 'operation success'
+			})
+		}
 	}
 }
 </script>
 
 <style>
 .container {
-	margin: 0 auto;
-	min-height: 100vh;
-	display: flex;
+	/* margin: 0 auto;
+	min-height: 100vh; */
+	/* display: flex;
 	justify-content: center;
 	align-items: center;
-	text-align: center;
+	text-align: center; */
 }
-
+/*
 .title {
 	font-family: 'Quicksand', 'Source Sans Pro', -apple-system,
 		BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,
@@ -53,5 +66,5 @@ export default {
 
 .links {
 	padding-top: 15px;
-}
+} */
 </style>
